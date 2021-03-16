63eb1c04b38dd0ab2c4840ca18b87d762f09160e
lsantos <lsantos.ls@outlook.com>
2021-03-15 22:53:58 -0300

    Ajustando conxeão com o banco de dados

---
+++ ormconfig.js
@@ -0,0 +1,33 @@
+console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
+module.exports = {
+  "type": "mysql",
+  "url": process.env.DATABASE_URL,
+  "entities": [
+    `./${process.env.BASE_URL
+    }/modules /**/typeorm/entities/*{.ts,.js}`
+  ],
+  "migrations": [
+    `./${process.env.BASE_URL}/shared/typeorm/migrations/*{.ts,.js}`
+  ],
+  "cli": {
+    "migrationsDir": "./src/shared/typeorm/migrations"
+  },
+}
+
+
+// {
+//   "entities": [
+//     "src/entity/**/*.ts"
+//   ],
+//   "migrations": [
+//     "src/migration/**/*.ts"
+//   ],
+//   "subscribers": [
+//     "src/subscriber/**/*.ts"
+//   ],
+//   "cli": {
+//     "entitiesDir": "src/entity",
+//     "migrationsDir": "src/migration",
+//     "subscribersDir": "src/subscriber"
+//   }
+// }
